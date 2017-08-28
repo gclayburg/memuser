ab02cf725576c1a074622049aa2a711923aa801f
Gary Clayburg <gclaybur@comcast.net>
2017-08-28 13:07:28 -0600

    create docker image with layers and using secure bootuser inside image

--- src/main/docker/bootrunner.sh
+++ src/main/docker/bootrunner.sh
@@ -7,12 +7,12 @@ date_echo(){
 
 if [ -d /app ]; then
   #execute springboot expanded jar, which may have been constructed from several image layers
-  date_echo "exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -cp org.springframework.boot.loader.JarLauncher $*"
-  exec "java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -cp /app org.springframework.boot.loader.JarLauncher $*"
+  date_echo "exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -cp /app org.springframework.boot.loader.JarLauncher $*"
+  exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -cp /app org.springframework.boot.loader.JarLauncher "$*"
 elif [ -f /app.jar ]; then
   # execute springboot jar
   date_echo "exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar /app.jar $*"
-  exec "java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar /app.jar $*"
+  exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar /app.jar "$*"
 else
   date_echo "springboot application not found in /app or /app.jar"
   exit 1
