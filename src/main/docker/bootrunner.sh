65b7ec8c0f4eb876abe34e36f7d490565b953852
Gary Clayburg <gclaybur@comcast.net>
2017-09-02 12:08:35 -0600

    process runtime arguments to docker image correctly

--- src/main/docker/bootrunner.sh
+++ src/main/docker/bootrunner.sh
@@ -8,12 +8,12 @@ if [ -d "${HOME}/app" ]; then
   #execute springboot expanded jar, which may have been constructed from several image layers
   date_echo "exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -cp ${HOME}/app org.springframework.boot.loader.JarLauncher $*"
   # shellcheck disable=SC2086
-  exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -cp "${HOME}/app" org.springframework.boot.loader.JarLauncher "$*"
+  exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -cp "${HOME}/app" org.springframework.boot.loader.JarLauncher "$@"
 elif [ -f "${HOME}/app.jar" ]; then
   # execute springboot jar
   date_echo "exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar ${HOME}/app.jar $*"
   # shellcheck disable=SC2086
-  exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar "${HOME}/app.jar" "$*"
+  exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar "${HOME}/app.jar" "$@"
 else
   date_echo "springboot application not found in ${HOME}/app or ${HOME}/app.jar"
   exit 1
