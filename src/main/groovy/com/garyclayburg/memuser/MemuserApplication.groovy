463f9ccb3c872eeb4d6658e2ba25ea3e7534778c
Gary Clayburg <gclaybur@comcast.net>
2021-06-27 12:38:07 -0500

    bump version to 0.8.2-SNAPSHOT

--- src/main/groovy/com/garyclayburg/memuser/MemuserApplication.groovy
+++ src/main/groovy/com/garyclayburg/memuser/MemuserApplication.groovy
@@ -13,7 +13,6 @@ import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
